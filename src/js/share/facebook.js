a742d0a5b79dd53f4ebd87d9d8ff1510282e7d70
kanakana0330 <kanakana0330@gmail.com>
2015-06-15 11:53:12 +0900

    add src

---
+++ src/js/share/facebook.js
@@ -0,0 +1,7 @@
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id; js.async = true;
+  js.src = "//connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v2.3";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
